08d13b0c489e1f9d03b8ad8c41d0bc172f821f1e
chiranjsutariya372 <chiranjsutariya372@gmail.com>
2024-02-01 18:09:32 +0530

    addition

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    
+    cout <<"Enter the A: ";
+    cin >> a;
+    
+    cout <<"Enter the B: ";
+    cin >> b;
+    
+    cout <<"Answer: " << a+b;
+
+    return 0;
+}
